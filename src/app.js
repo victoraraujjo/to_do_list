094f9c1d2e0d9734b07529afc67ea65e36dafb16
Victor <victorcostaraujo4@gmail.com>
2025-01-17 19:58:06 -0300

    feat: integrate database connection in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const dbConnection = require('./database/connection')
+
 const app = express()
 
 const PORT = 8080
 
 app.listen(PORT, () => {
     console.log('running')
+    dbConnection.connectToDatabase()
 })
